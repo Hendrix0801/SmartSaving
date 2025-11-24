006bf512a73b9d1930aa88ad3179fa52fb425c38
Hendrix <taytaytayhig.gmail.com>
2025-11-25 00:05:06 +0800

    Initial Commit

---
+++ SmartSaving/SmartSavingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSavingApp.swift
+//  SmartSaving
+//
+//  Created by 刘文辉 on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartSavingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
